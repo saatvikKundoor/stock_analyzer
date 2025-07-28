90b7b4bff294efab94d514ad5905d69155171412
Saatvik Kundoor <221399230+saatvikKundoor@users.noreply.github.com>
2025-07-28 03:58:38 -0500

    Created basic structure. Imported libraries and structured functions

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import yfinance as yf 
+
+def get_user_inputs()
+    #user inputs for invested amount, hold duration, expected returns for bull/bear/base
+
+def calculate_returns()
+    #returns pre and post tax returns based on inputs
+
+def fetch_sp500()
+    #compare data with S&P
+
+def display_result()
+    #plot data comparisons
