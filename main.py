b7eb2e5a4b810270f22eb3dd83c0474a5e92088b
Saatvik Kundoor <221399230+saatvikKundoor@users.noreply.github.com>
2025-07-28 18:48:49 -0500

    Fixed format errors

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import yfinance as yf 
 
-def get_user_inputs()
+def get_user_inputs():
     #user inputs for invested amount, hold duration, expected returns for bull/bear/base
+    invested_amount = float(input("How much are you investing?"))
 
-def calculate_returns()
+def calculate_returns():
     #returns pre and post tax returns based on inputs
 
-def fetch_sp500()
+#def fetch_sp500():
     #compare data with S&P
 
-def display_result()
+#def display_result():
     #plot data comparisons
+    #hbkhkj
