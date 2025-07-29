c217ccd3d25346efc8220619770acd4250184bea
Saatvik Kundoor <221399230+saatvikKundoor@users.noreply.github.com>
2025-07-28 19:06:48 -0500

    Finished basic user input function

--- main.py
+++ main.py
@@ -6,13 +6,27 @@ import yfinance as yf
 def get_user_inputs():
     #user inputs for invested amount, hold duration, expected returns for bull/bear/base
     invested_amount = float(input("How much are you investing?"))
+    hold_dur = int(input("How long will you be holding?"))
+    bull_return = float(input("What is the expected return in a bullish market?"))
+    bear_return = float(input("What is the expected return in a bearish market?"))
+    base_return = float(input("What is the expected return in a bullish market?"))
 
 def calculate_returns():
     #returns pre and post tax returns based on inputs
+    pass
 
-#def fetch_sp500():
-    #compare data with S&P
+def fetch_sp500():
+    pass
 
-#def display_result():
+def display_result():
     #plot data comparisons
-    #hbkhkj
+    pass
+
+def main():
+    get_user_inputs()
+    calculate_returns()
+    fetch_sp500()
+    display_result()
+
+main()
+
