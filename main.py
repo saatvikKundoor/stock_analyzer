663de1c337512e429c43fb47a828c2f6bf546f22
Saatvik Kundoor <221399230+saatvikKundoor@users.noreply.github.com>
2025-07-28 21:06:40 -0500

    Fintetuned user input function + fixed variable logic errors

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import yfinance as yf 
 
+invested_amount = 0
+hold_dur = 0
+bull_return = 0
+bear_return = 0
+base_return = 0
 def get_user_inputs():
     #user inputs for invested amount, hold duration, expected returns for bull/bear/base
     invested_amount = float(input("How much are you investing?"))
-    hold_dur = int(input("How long will you be holding?"))
-    bull_return = float(input("What is the expected return in a bullish market?"))
-    bear_return = float(input("What is the expected return in a bearish market?"))
-    base_return = float(input("What is the expected return in a bullish market?"))
-
+    hold_dur = int(input("How long will you be holding (years)?"))
+    bull_return = float(input("What is the expected return in a bullish market? (percent change)"))
+    bear_return = float(input("What is the expected return in a bearish market? (percent change)"))
+    base_return = float(input("What is the expected return in a bullish market? (percent change)"))
+    return(invested_amount,hold_dur,bull_return,bear_return,base_return)
+invested_amount,hold_dur,bull_return,bear_return,base_return = get_user_inputs()
 def calculate_returns():
     #returns pre and post tax returns based on inputs
+    print(invested_amount)
     pass
 
 def fetch_sp500():
@@ -23,7 +30,7 @@ def display_result():
     pass
 
 def main():
-    get_user_inputs()
+    #get_user_inputs()
     calculate_returns()
     fetch_sp500()
     display_result()
